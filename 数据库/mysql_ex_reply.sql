2ea75db6bb0e5e2ed76ace2d6de9d97965c7043b
orzt <zhangtao@orzt.com>
2019-01-06 00:06:33 +0800

    一篇关于元类的译文，一则关于mysql的练习题解答

---
+++ 数据库/mysql_ex_reply.sql
@@ -0,0 +1,274 @@
+use workman;
+
+-- student; sid, sname, sage(datetime), ssex    学生表
+-- 			sc:  sid,   cid, score              成绩表
+-- 	course: cid, cname, tid                     课程表
+-- teacher: tid tname                           教师表
+
+## 练习题目
+-- 1. 查询" 01 "课程比" 02 "课程成绩高的学生的信息及课程分数
+select s.*, a.score as score_01, b.score as score_02 from student as s,
+(select sid, score from sc where cid = "01") as a,
+(select sid, score from sc where cid = "02") as b
+where a.score > b.score and a.sid = b.sid and a.sid = s.sid; -- 把01课程的成绩和02课程的成绩筛选出来，然后再联结
+
+-- 1.1 查询同时参加了" 01 "课程和" 02 "课程的学生
+select s.*, sc1.score as score_01, sc2.score as score_02 from student as s,
+(select sid, score from sc where cid = "01") as sc1,
+(select sid, score from sc where cid = "02") as sc2
+where sc1.sid = sc2.sid and sc1.sid = s.sid; -- 在上一则查询的基础上，减去比较条件
+
+-- 1.2 查询参加了" 01 "课程但可能未参加" 02 "课程的学生(未参加时显示为 null )
+select s.*, sc1.score as score_01, sc2.score as score_02 from student as s
+left join (select sid, score from sc where cid = "01") as sc1 on sc1.sid = s.sid 
+left join (select sid, score from sc where cid = "02") as sc2
+on sc1.sid = sc2.sid; -- 在上一则查询的基础上，使用左联结代替
+
+-- 1.3 查询未参加" 01 "课程但参加了" 02 "课程的学生
+select s.* from student as s, 
+(select sid, min(cid) as min_cid from sc group by sid having min_cid = "02") as tempsc
+where s.sid = tempsc.sid; -- 计算课程id的最小值，需为02
+
+-- 2. 查询平均成绩大于等于 60 分的同学的学生编号和学生姓名和平均成绩
+select s.sid, s.sname, sc.avg_score from student as s,
+(select sid, avg(score) as avg_score from sc group by sid having avg_score > 60) as sc
+where s.sid = sc.sid; -- 先筛选出平均分大于60的所有学生id级平均分，再联结
+
+-- 3. 查询在 SC 表存在成绩的学生信息
+select s.* from student as s where exists (select 1 from sc where sid = s.sid); -- 存在判断
+
+select distinct student.* from student, sc where student.sid = sc.sid; -- 联结去重
+
+-- 4. 查询所有同学的学生编号、学生姓名、选课总数、所有课程的总成绩(没成绩的显示为 null )
+select s.sid, s.sname, tempsc.totalsc, tempsc.sumscore from student as s left join
+(select sid, count(cid) as totalsc, sum(score) as sumscore from sc group by sid) as tempsc
+on s.sid = tempsc.sid; -- 先处理sc表计算出需要的字段，再左联结进行整理
+
+-- 4.1 查有成绩的学生信息
+select s.* from student as s right join (select sid, sum(score) as sumscore from sc group by sid) as tempsc
+on s.sid = tempsc.sid and tempsc.sumscore > 0; -- 总成绩肯定大于0
+
+select * from student where exists (select 1 from sc where student.sid = sid); -- 在sc表中的sid即为有成绩的学生
+
+-- 5. 查询「李」姓老师的数量
+select count(tid) as num from teacher where tname like "李%";
+
+select count(tid) as num from teacher where tname regexp "^李";
+
+-- 5.1 查询「李」姓学生的数量
+select count(sid) as num from student where sname like "李%"; -- 通配符匹配
+
+select count(sid) as num from student where sname regexp "^李"; -- 正则匹配
+
+-- 6. 查询学过「张三」老师授课的同学的信息 
+-- (需要使用到4张表)
+select s.*, sc.cid, t.tid, t.tname from student as s, sc, course, teacher as t where
+s.sid = sc.sid and sc.cid = course.cid and course.tid = t.tid and t.tname = "张三";
+
+-- 7. 查询没有学全所有课程的同学的信息 
+select s.* from student as s,
+(select sid, count(cid) as num from sc group by sid having num < (select count(cid) from course)) as tempsc
+where s.sid = tempsc.sid; 
+-- 但这种解法得出来的结果不包括什么课都没选的同学 --
+-- 下面的这种解法考虑了没选课的情况 --
+select s.*, tempsc.num from student as s left join
+(select sid, count(cid) as num from sc group by sid) as tempsc
+on s.sid = tempsc.sid where (tempsc.num is null or (tempsc.num < (select count(cid) from course))); 
+
+-- 8. 查询至少有一门课与学号为" 01 "的同学所学相同的同学的信息 
+select distinct student.* from student, sc as outsc
+where outsc.sid = student.sid and 
+exists(select sc.cid from sc where sc.sid = "01" and sc.cid = outsc.cid); -- 先找出01同学所学的所有课程
+
+-- 9. 查询和" 01 "号的同学学习的课程完全相同的其他同学的信息 
+-- 思路：1.首先交叉联结，假设所有的学生都有数量相同的课程
+--            2.然后联结的结果继续与sc左联结，目的是可以筛选出那些实际上未选择某课程的学生id
+--            3.内联结student以此获取满足要求的学员数据
+-- select student.sid, t.cid from student, (select cid from sc where sid = "01") as t;
+-- select t2.sid sc.sid, sc.cid from (select student.sid, t.cid from student, (select cid from sc where sid = "01") as t) as t2 left join sc on t2.sid = sc.sid and t2.cid = sc.cid where sc.sid is null
+select * from student where sid not in(
+select t2.sid from (select student.sid, t.cid from student, (select cid from sc where sid = "01") as t)
+as t2 left join sc on t2.sid = sc.sid and t2.cid = sc.cid where sc.sid is null) and sid != "01";
+-- 1 1    1 1
+-- 1 2    1 null
+-- 1 3    1 3
+-- 叠合，所以右侧是不符合要求的
+
+-- 10. 查询没学过"张三"老师讲授的任一门课程的学生姓名 
+-- 思路：1.张三老师讲授的所有课程
+--      2.学过张三老师课的学生，然后取反
+select student.sid from student where student.sid not in (
+    select sid from sc where exists(
+        select 1 from course, teacher where teacher.tname = "张三" and teacher.tid = course.tid and course.cid = sc.cid
+    )
+);
+
+-- 11. 查询两门及以上不及格课程的同学的学号，姓名及其平均成绩 
+select student.sid, student.sname, t2.avg_score from student,                  -- 提供对应的姓名
+(select sid from sc where score < 60 group by sid having count(cid) > 1) as t, -- 提供符合要求的同学sid
+(select sid, avg(score) as avg_score from sc group by sid) as t2               -- 提供对应的平均成绩
+where student.sid = t.sid and t.sid = t2.sid;
+
+-- 12. 检索" 01 "课程分数小于 60，按分数降序排列的学生信息
+select student.*, sc.score from student, sc where sc.sid = student.sid and sc.cid = "01" and sc.score < 60 order by sc.score desc;
+
+-- 13. 按平均成绩从高到低显示所有学生的所有课程的成绩以及平均成绩
+select sid, sum(score) as sum_score, avg(score) as avg_score from sc group by sid order by sum_score desc, avg_score desc;
+
+-- 14. 查询各科成绩最高分、最低分和平均分：
+--     以如下形式显示：课程 ID，课程 name，选修人数 num, 最高分，最低分，平均分，及格率，中等率，优良率，优秀率
+--     及格为>=60，中等为：70-80，优良为：80-90，优秀为：>=90
+--     查询结果按人数降序排列，若人数相同，按课程号升序排列
+-- 思路：使用case语句 将分组后的每条记录的分数转为 1 or 0,再使用sum求和
+select course.cname, std_sc_situation.* from
+(select cid, count(sid) as num, max(score), min(score), avg(score),
+sum(case when score >= 60 then 1 else 0 end) / count(sid) as rate_60,
+sum(case when score >= 70 and score < 80 then 1 else 0 end) / count(sid) as rate_70,
+sum(case when score >= 80 and score < 90 then 1 else 0 end) / count(sid) as rate_80,
+sum(case when score >= 90 then 1 else 0 end) / count(sid) as rate_90 from sc
+group by cid order by num desc, cid) as std_sc_situation, course
+where std_sc_situation.cid = course.cid;
+
+-- 15. 按各科成绩进行排序，并显示排名， Score 重复时保留名次空缺
+select cid, score, @currank := @currank + 1 as rankid from
+(select @currank := 0) as t, sc order by score desc;
+
+-- 15.1 按各科成绩进行排序，并显示排名， Score 重复时合并名次（比如1，2，3，3，3，4）
+select sc.cid, score,
+case when @frontscore = score then @currank
+    when @frontscore := score then @currank := @currank + 1 end as rankid
+from (select @currank := 0, @frontrank := null) as t, sc order by score desc;
+
+-- 16.  查询学生的总成绩，并进行排名，总分重复时保留名次空缺(比如1，2，3，4，5，6）
+select t1.*, @currank := @currank+1 as rankid from (
+select sc.SId, sum(score) from sc GROUP BY sc.SId ORDER BY sum(score) desc) as t1,
+(select @currank:=0) as t;
+
+-- 16.1 查询学生的总成绩，并进行排名，总分重复时不保留名次空缺
+select t1.*,
+case when @lasttotalscore = t1.totalscore then @currank
+    when @lasttotalscore := t1.totalscore then @currank := @currank + 1 end as rankid
+from (select @lasttotalscore := null, @currank := 0) as t2,
+(select sid, sum(score) as totalscore from sc group by sid order by totalscore desc) as t1;
+
+-- 17. 统计各科成绩各分数段人数：课程编号，课程名称，[100-85]，[85-70]，[70-60]，[60-0] 及所占百分比
+select course.cname, std_sc.* from course,
+(select cid, count(sid) as student_num,
+sum(case when score < 60 then 1 else 0 end) as num_60,
+concat(sum(case when score < 60 then 1 else 0 end) / count(sid) * 100, "%") as rate_num_60,
+sum(case when score < 70 and score >= 60 then 1 else 0 end) as num_70,
+concat(sum(case when score < 70 and score >= 60 then 1 else 0 end) / count(sid) * 100, "%") as rate_num_70,
+sum(case when score < 85 and score >= 70 then 1 else 0 end) as num_85,
+concat(sum(case when score < 85 and score >= 70 then 1 else 0 end) / count(sid) * 100, "%") as rate_num_85,
+sum(case when score <= 100 and score >= 85 then 1 else 0 end) as num_100,
+concat(sum(case when score <= 100 and score >= 85 then 1 else 0 end) / count(sid) * 100, "%") as rate_num_100
+from sc group by cid) as std_sc where course.cid = std_sc.cid;
+
+-- 18. 查询各科成绩前三名的记录
+-- 思路：大于前三名的成绩数量一定小于3
+select * from sc where (select count(sid) from sc as t where sc.cid = t.cid and t.score > sc.score) < 3 order by cid, score desc;
+
+-- 19. 查询每门课程被选修的学生数(每门课的选课人数)
+select cid, count(sid) as num from sc group by cid order by num desc;
+
+-- 20. 查询出只选修两门课程的学生学号和姓名
+select student.sname, student.sid from student, (select sid, count(cid) as num from sc group by sid having num = 2) as t
+where student.sid = t.sid;
+
+-- 21. 查询男生、女生人数
+select count(sid) as num from student group by ssex;
+
+-- 22. 查询名字中含有「风」字的学生信息
+select * from student where sname regexp "风";
+select * from student where sname like "%风%";
+
+-- 23. 查询同名同性学生名单，并统计同名人数
+select a.* from student as a, student as b where a.sname = b.sname and a.ssex = b.ssex and a.sid != b.sid;
+select student.* from student right join (select sname, ssex, count(sid) as 同名人数 from student group by sname, ssex having 同名人数 > 1) as t
+on student.sname = t.sname and student.ssex = t.ssex;
+
+-- 24. 查询 1990 年出生的学生名单
+select student.* from student where student.sage regexp "^1990";
+
+-- 25. 查询每门课程的平均成绩，结果按平均成绩降序排列，平均成绩相同时，按课程编号升序排列
+select cid, avg(score) as avg_score from sc group by cid order by avg_score desc, cid;
+
+-- 26. 查询平均成绩大于等于 85 的所有学生的学号、姓名和平均成绩 
+select t.sid, sname, t.avg_score from student, (select sid, avg(score) avg_score from sc group by sid having avg_score >= 85) as t
+where t.sid = student.sid;
+
+-- 27. 查询课程名称为「数学」，且分数低于 60 的学生姓名和分数 
+select student.sname, sc.score, sc.score from sc, course, student
+where sc.cid = course.cid and course.cname = "数学" and 
+sc.score < 60  and student.sid = sc.sid;
+
+-- 28. 查询所有学生的课程及分数情况（存在学生没成绩，没选课的情况）
+select student.*, sc.cid, case when sc.score is null then 0 else sc.score end as score from student left join sc on student.sid = sc.sid
+order by student.sid, sc.score desc; -- 这里我额外使用了case语句，其实就要求而言是画蛇添足的
+
+-- 29. 查询任何一门课程成绩在 70 分以上的姓名、课程名称和分数
+select student.*, course.cname, sc.score from student, sc, course where student.sid = sc.sid and course.cid = sc.cid
+and sc.score > 70;
+
+-- 30. 查询不及格的课程
+select distinct cid from sc where score < 60;
+
+-- 31. 查询课程编号为 02 且课程成绩在 80 分以上的学生的学号和姓名
+-- 内联结也可
+select * from student where sid in (select distinct sid from sc where cid = "02" and score > 80);
+
+-- 32. 求每门课程的学生人数 
+select cid, count(sid) as num from sc group by cid;
+
+-- 33. 成绩不重复，查询选修「张三」老师所授课程的学生中，成绩最高的学生信息及其成绩
+select student.*, teacher.tname, course.cname, sc.score from student, sc, course, teacher
+where student.sid = sc.sid and sc.cid = course.cid and course.tid = teacher.tid and teacher.tname = "张三" 
+order by sc.score desc limit 1; 
+
+-- 34. 成绩有重复的情况下，查询选修「张三」老师所授课程的学生中，成绩最高的学生信息及其成绩
+select student.*, sc1.score from sc as sc1, student
+where student.sid = sc1.sid and (select count(sid) from sc as sc2 where sc1.cid = sc2.cid and sc2.score > sc1.score) < 1
+and sc1.cid = (select distinct sc.cid from sc, course, teacher where sc.cid = course.cid and course.tid = teacher.tid and teacher.tname = "张三");
+
+-- 下面这种方法是预先计算出排名，然后再取排名id为1的值，和上面的其实本质是一样的
+select student.*,t1.score
+from student INNER JOIN (select sc.SId,sc.score,
+case when @fontage=sc.score then @rankid when @fontage:=sc.score then @rankid := @rankid + 1 end  as rankid
+from course ,teacher ,sc,(select @fontage:=null,@rankid:=0) as t
+where course.CId=sc.CId
+and course.TId=teacher.TId
+and teacher.Tname='张三'
+ORDER BY sc.score DESC) as t1 on student.SId=t1.SId
+where t1.rankid=1;
+
+-- 35. 查询不同课程成绩相同的学生的学生编号、课程编号、学生成绩
+select  student.sname, result.sid, sc.cid, sc.score from student, sc,
+(select distinct sc1.sid from sc as sc1, sc as sc2 where sc1.sid = sc2.sid and sc1.cid != sc2.cid and sc1.score = sc2.score) as result
+where student.sid = sc.sid and sc.sid =  result.sid;
+
+-- 36. 查询每门功课成绩最好的前两名
+select * from sc as sc1 where (select count(sid) from sc as sc2 where sc2.cid = sc1.cid and sc2.score > sc1.score) < 2 order by sc1.cid, sc1.score desc;
+
+-- 37. 统计每门课程的学生选修人数（超过 5 人的课程才统计）。
+select cid, count(sid) as num from sc group by cid having num > 5;
+
+-- 38. 检索至少选修两门课程的学生学号 
+select sid, count(cid) as num from sc group by sid having num > 1;
+
+select distinct t1.sid from sc as t1 where (select count(* )from sc where t1.sid=sc.sid)>=2;
+
+-- 39. 查询选修了全部课程的学生信息
+select student.* from student, (select sid from sc group by sid having count(cid) = (select count(cid) from course))
+as result where student.sid = result.sid;
+
+-- 40. 按照出生日期来算，当前月日 < 出生年月的月日则，年龄减一
+select sname, sid, timestampdiff(year, sage, curdate()) as age from student;
+
+
+-- student; sid, sname, sage(datetime), ssex
+-- 			sc: sid, cid, score
+-- 	course: cid, cname, tid
+-- teacher: tid tname
+
+
+
---
+++ 译文/(译文)what's_metaclass.md
@@ -0,0 +1,335 @@
+﻿翻译原文来自于https://stackoverflow.com/questions/100003/what-are-metaclasses-in-python 一个高赞回答
+
+**
+
+## 类是一种对象
+
+**
+
+在说明元类之前，我们需要先掌握什么是类。Python对于类的定义很特别，借鉴了smalltalk语言。
+包括Python在内的大多数语言中，类只是一段描述如何创建对象的代码。
+
+    class ObjectCreator(object):
+        pass
+    my_object = ObjectCreator()
+    print(my_object)
+
+事实上Python的类远不仅如此，类还是对象。
+
+一旦使用class关键字，Python就会创建出一个对象，
+下例代码：
+
+    class ObjectCreator(object):
+        pass
+
+在内存中创建了一个名为ObjectCreator的对象。
+
+这个对象可以用来创建对象，这也是为什么它是一个类。
+本质上，类还是一个对象，因此：
+ 1. 可以将类赋值给一个变量
+ 2. 可以复制类
+ 3. 可以向类添加属性
+ 4. 可以把类作为参数传递给函数
+
+例如：
+
+    print(ObjectCreator)
+    def echo(o):
+        print(o)
+    echo(ObjectCreator)
+    print(hasattr(ObjectCreator, "new_attribute"))
+    ObjectCreator.new_attribute = "foo"
+    print(ObjectCreator.new_attribute)
+    ObjectCreatorMirror = ObjectCreator
+    print(ObjectCreatorMirror.new_attribute)
+    print(ObjectCreatorMirror())
+
+**
+
+## 动态创建类
+
+**
+
+既然类属于对象，那么就可以随时创建了。
+首先，使用class关键字创建一个类：
+
+    def choose_class(name):
+        if name == "foo":
+            class Foo(object):
+                pass
+            return Foo
+        else:
+            class Bar(object):
+                pass
+            return Bar
+    MyClass = choose_class("foo")
+    print(MyClass)
+    print(MyClass())
+
+但是，这不算动态创建，因为我们仍然需要事先写下整个类。
+既然类属于对象，那么它一定是被某样东西生成的。
+当我们使用class关键字，Python就自动创建出了一个对象。事实上Python中的大多数东西，我们都可以手动来完成。
+
+还记得 type 吗？这个函数能让你知道对象的类型是什么：
+
+    print(type(1))
+    print(type("1"))
+    print(type(ObjectCreator()))
+
+是的，type还有一种功能，它可以动态创建类。type接收class的相关描述作为参数，然后返回需要的class。
+(我知道，Python中一个函数传入不同的参数便有不同的用法这种设计很不好，这个问题是Python向后兼容导致的。)
+
+type的语法如下：
+
+    type(name of the class,
+        tuple of the baseClass,
+        dictionary containing attributes names and values)
+
+举个栗子：
+
+    class MyShinyClass(Object):
+        pass
+
+上面的对象(类)可以通过手动方式创建：
+
+    MyShinyClass = type("MyShinyClass", (), {})
+    print(MyShinyClass)
+    print(MyShinyClass())
+
+你可能注意到了，这里的"MyShinyClass"这个类名和左侧的变量名一样。虽然它们可以不一样，但我们没有必要去复杂化事情。
+
+type 第三个参数是一个字典类型，用来表示类的属性。比如：
+
+    class Foo(object):
+        bar = True
+
+可以被表示为：
+
+    Foo = type("Foo", (), {"bar":True})
+
+使用起来没什么两样：
+
+    print(Foo)
+    print(Foo.bar)
+    f =Foo()
+    print(f)
+    print(f.bar)
+
+当然，这个类可以作为基类：
+
+    class FooChild(Foo):
+        pass
+    FooChild = type("FooChild", (Foo,), {})
+    print(FooChild)
+    print(FooChild.bar)
+
+我们还可以向类添加方法。定义一个正常的函数然后赋予类。
+
+    FooChild = type("FooChild", (Foo,), {"echo_bar": lambda self: self.bar})
+    hasattr(Foo, "echo_bar")
+    hasattr(FooChild, "echo_bar")
+    my_foo = FooChild()
+    my_foo.echo_bar()
+
+我们甚至可以在创建类以后，再添加方法，和平常没区别。
+
+    FooChild.echo_bar_more = lambda self: "yet another method"
+    hasattr(FooChild, "echo_bar_more")
+
+你大概知晓了我们接下来要了解什么了：类属于一种对象，可以随时动态创建。
+这就是我们使用class关键字时，Python所做的工作，使用元类metaclass来完成。
+
+**
+
+## 重点来了：什么是元类？
+
+**
+
+元类就是创建出类的东西。
+我们定义一个类是为了创建对象，对吗？
+但我们知道类也是对象。
+是的，元类就是创建这些类的东西。它们就是类对象的类，可以这样想象：
+
+    MyClass = MetaClass()
+    my_object = MyClass()
+
+我们已经知道type可以：
+
+    MyClass = type("MyClass", (), {})
+
+这是因为我们以为的函数type实际上是一个可调用的元类，type就是Python在屏幕后面用来创建所有类的元类。
+
+现在你是否很好奇type首字母没有大写呢？
+嗯...我猜这是为了和str，int 保持一致性，后两者分别创建string对象和integer对象，而type创建出所有的类对象。
+我们可以看下它的__class_属性。
+Python中的一切都是对象。比如ints，strings，functions，classes。这些都是通过类创建出来的：
+
+    number = 520
+    number.__class__
+    name = "haoke"
+    name.__class__
+    def foo(): pass
+    foo.__class__
+    class Bar(object): pass
+    b = Bar()
+    b._class__
+
+那么，上面这些_class__的__class__是什么呢？
+
+    number.__class__.__class__
+    name.__class__.__class__
+    foo.__class__.__class__
+    b.__class__.__class__
+
+(上面的结果在Python3中都是<class 'type'>)
+没错，元类就是用来创建类的东西。
+你可以把元类当成是"类工厂"。
+type是Python内置的元类，我们也可以创建自己的元类。
+
+py2的__metaclass__属性和py3的metaclass关键字
+
+下面一一道来。
+Python2：
+    在定义类的时候，我们可以添加一个__metaclass__属性：
+    class Foo(object):
+        __metaclass__ = something...
+        [...]
+    这样写，Python会使用元类来创建这个这个Foo类。
+    但要小心，这不简单。
+    虽然已经写了class Foo(object)，但Foo类此时还未创建。
+    Python会先检查类是否定义了__metaclass__，如果有，它就会用指定的元类来创建这个类Foo。但若没有，就使用默认的type来创建类Foo。
+
+    多看几次：
+    class Foo(Bar, Rab):
+        pass
+    Python会做如下操作：
+    1.Foo中定义了__metaclass_属性吗？
+    2.定义了的话，就使用指定的元类的创建这个类。
+    3.如果没有定义，Python就会在当前模块中查找，然后重新进行 属性定义 判断(但只适用于不继承任何东西的类，基本上是老式类)。
+    4.如果Python没有找到任何关于__metaclass__的显式定义，它就会使用第一个父类(Bar)的元类(可能是默认值type)来创建这个类。
+    注意，__metaclass__属性不会被继承，但父类(Bar.__class__)的元类会被继承。如果Bar使用__metaclass__属性，该属性使用type()(而不是type.__new__())创建Bar，那么子类将不会继承该行为。
+    (这段问答来自于回答下方：
+        Be careful here that the __metaclass__ attribute will not be inherited, the metaclass of the parent (Bar.__class__) will be. If Bar used a __metaclass__ attribute that created Bar with type() (and not type.__new__()), the subclasses will not inherit that behavior. Can someone please explain this a little bit deeper? Would really appreciate some help here. – Deep Jun 25 '17 at 14:43 
+        
+        @Deep: The actual metaclass of a class is specified in .__class__, whereas .__metaclass__ specifies which callable should be used to alter the class during creation. If for example .__metaclass__ contains a function foo_bar() that uses type(x,y,z) to alter the class, then you will have .__metaclass__ = foo_bar which will not be inherited but .__class__ will be type, because that's what was used to create the new altered class. Read this a few times. I am 99% sure I haven't made a mistake ;) – Philip Stark Aug 16 '17 at 9:17
+    )
+    
+现在有一个很大的问题摆在这：我们可以向__metaclass__属性**赋什么值**呢？
+答案是：能创建类的东西，type或其子类。
+
+Python3:
+    class Foo(object, metaclass=something):
+        [...]
+    在3中，__metaclass__属性已经不再使用了，但作用表现上大致相同。
+
+**
+
+## 自定义元类
+
+**
+（这部分代码是针对Python2.x的)
+一个元类的作用是自动改变将被创建出来的类。
+想象一个简单的场景，你想某个模块下的所有类都应该大写化它们的属性名。这有许多种方法来完成，但其中有一种是在模块中定义__metaclass__属性。
+这种情况下，这个模块下的所有类都会使用这个指定的元类来创建。
+我们要告诉元类所有的类属性都应该大写化，幸好，__metaclass__实际上是可以被调用的，其值不一定非要是类。
+
+    def upper_attr(futture_class_name, future_class_parents, future_class_attr):
+        '''return a class object, with the list of its attribute turned intor uppercase'''
+        # pick up any attribute that does not start with "__" and uppercase it
+        uppercase_attr = {}
+        for name, val in future_class_attr.items():
+            if not name.startswith("__"):
+                uppercase_attr[name.upper()] = val
+            else:
+                uppercase_attr[name] = val
+        # let "type" do the class creation
+        return type(future_class_name, future_class_parents, uppercase_attr)
+    
+    __metaclass__ = upper_attr # this will affect all classes in the module
+    
+    class Foo(): # global __metaclass__ won't work with "object" though.
+        # but we can define __metaclass__ here instead to affect only this class.
+        # and this will work  with "object" children.
+        bar = "bip"
+    
+    print(hasattr(Foo, "bar")
+    print(hasattr(Foo, "BAR"))
+    f = Foo()
+    print(f.BAR)
+
+现在，让我们使用真正的元类来做同样的事情
+记住：‘type'实际上是一个类似str和int的类，仅仅没有大写开头。
+   
+    class UpperAttrMetaClass(type):
+        # __new__ 是要先于 __init__ 被调用
+        # 它是类的构造方法，会返回一个实例
+        # 而 __init__ 只是在之后对实例进行初始化操作。
+        # 你几乎用不到 __new__ 方法，除非你想控制实例的创建过程
+        # 在这里，创建的实例是类，我们想自定义它，
+        # 所以 我们需要覆盖 __new__
+        # 有些人会选择覆盖__call__方法来达到同样的效果，但我们在这不会这样做
+        def __new__(upperattr_metaclass, future_class_name,
+                    future_class_parents, future_class_attr):
+            uppercase_attr = {}
+            for name, val in future_class_attr.items():
+                if not name.startswith("__"):
+                    uppercase_attr[name.upper()] = val
+                else:
+                uppercase_attr[name] = val
+            return type(future_class_name, future_class_parents, future_class_attr)
+    
+   
+
+ 但上面的代码并不OOP，我们直接调用的type，我们没有调用其父类的__new__方法：
+    
+    class UpperAttrMetaclass(type):
+        def __new__(upperattr_metaclass, future_class_name,
+                    future_class_parents, future_class_attr):
+            uppercase_attr = {}
+            for name, val in future_class_attr.items():
+                if not name.startswith('__'):
+                    uppercase_attr[name.upper()] = val
+                else:
+                    uppercase_attr[name] = val
+            return type.__new__(upperattr_metaclass, future_class_name,
+                                future_class_parents, uppercase_attr)
+
+你可能注意到了upperattr_metaclass这个额外的参数。这没有什么特别的：
+__new__ 总是会收到定义的这个类作为第一个参数，就像普通的实例方法中有self参数一样，它接收的是一个实例，
+类方法中的cls参数一样，接收的是一个类。
+简化下上面的代码：
+
+    class UpperAttrMetaclass(type):
+        def __new__(cls, clsname, bases, dct):
+            uppercase_attr = {}
+            for name, val in dct.items():
+                if not name.startswith('__'):
+                    uppercase_attr[name.upper()] = val
+                else:
+                    uppercase_attr[name] = val
+            return type.__new__(cls, clsname, bases, uppercase_attr)
+
+我们可以使用super来使其更加正确，super会保证继承正确化(防止钻石继承的出现，防止继承查找没有按照mro来进行)：
+
+    class UpperAttrMetaclass(type):
+        def __new__(cls, clsname, bases, dct):
+            uppercase_attr = {}
+            for name, val in dct.items():
+                if not name.startswith('__'):
+                    uppercase_attr[name.upper()] = val
+                else:
+                    uppercase_attr[name] = val
+            return super(UpperAttrMetaclass, cls).__new__(cls, clsname, bases, uppercase_attr)
+
+这就是元类的所有了。
+
+真的，元类对于创建"黑魔法"真的很有用，但会复杂化事情。就其本身而言，还是很简单的：
+    * **拦截(类的创建)**
+    * **修改(类)**
+    * **返回(修改后的类)**
+    
+再精简一点就是：钩子、修改
+
+
+
+
